97099b0998a5b034538d0526114bb4620c76dc13
Rodrigo Negreiros <rodrigonegreiros@MacBook-Pro-de-Rodrigo.local>
2021-03-26 10:55:13 -0300

    primeiro envio

---
+++ server.js
@@ -0,0 +1,12 @@
+/* Esse arquivo vai servir tanto para prover os arquivos
+estáticos quanto os dinâmicos*/
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.listen(8080, () => console.log('Executando..')) // para inciar: npm start
